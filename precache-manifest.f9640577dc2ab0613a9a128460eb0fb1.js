95283b2ffd148cee97bc292a3cd2dceaef897fea
tetianasabanina <tatiana08st@gmail.com>
2019-10-18 10:09:40 +0300

    Updates

---
+++ precache-manifest.f9640577dc2ab0613a9a128460eb0fb1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "498398fcc020c2592161007753fb24a9",
+    "url": "/react-apps-counter/index.html"
+  },
+  {
+    "revision": "56120155aba24ef3ae14",
+    "url": "/react-apps-counter/static/css/main.a458ca52.chunk.css"
+  },
+  {
+    "revision": "b14d37ff6b74c33a2e79",
+    "url": "/react-apps-counter/static/js/2.bd025bb9.chunk.js"
+  },
+  {
+    "revision": "56120155aba24ef3ae14",
+    "url": "/react-apps-counter/static/js/main.c485d038.chunk.js"
+  },
+  {
+    "revision": "ba5e03bc297dc81d1597",
+    "url": "/react-apps-counter/static/js/runtime~main.19b34ae7.js"
+  }
+]);
